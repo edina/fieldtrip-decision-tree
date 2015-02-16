87fe5f3430fb41882b08e6af95500815b4a1869e
Ruben Gamez <ruben.gamez@ed.ac.uk>
2015-02-16 10:58:28 +0000

    Move the transversing logic to the eo-graph module Use cached templates for rendering the questions

--- src/www/js/decision-tree.js
+++ src/www/js/decision-tree.js
@@ -29,282 +29,143 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 DAMAGE.
 */
 
-"use strict";
+'use strict';
 
-/* jshint multistr: true */
+/* global _ */
 
-define(['records', 'utils', 'file'], function(records, utils, file){
-    //the first page
-    var page = "root";
-    //the object of the current question
-    var questionObj;
-    //an array of all the answers
-    var answers = [];
-    //the number of times that previous button is pressed, need restriction
-    var previous = 0;
-    //the tree questions
-    var questions, dgroup, dtype;
+define(['records', 'utils', 'file', './ext/eo-graph'], function(records, utils, file, EOGraph) {
+    var dgroup, dtype;
+    var eoGraph;
 
     var pluginRoot = 'plugins/decision-tree/';
 
     var dtreeId = 'fieldcontain-dtree-1';
 
-    var dtreeToStr = function(tree) {
-        var answers;
+    var fieldsetWidget = _.template(
+        '<fieldset data-role="controlgroup">' +
+            '<legend><%- question %></legend>' +
+            '<%= fields %>' +
+        '</fieldset>'
+    );
 
-        answers = tree.map(function(item) {
-            if (item.answer) {
-                return item.answer.value;
+    var radioWidget = _.template(
+        '<label for="radio-<%- id %>"><%- label %></label>' +
+        '<input ' +
+            'name="<%= name %>" ' +
+            'id="radio-<%= id %>" ' +
+            'value="<%= value %>" ' +
+            'type="radio" ' +
+            '<% if (required === true) { %>required="required"<% } %> ' +
+            '<% if (checked === true) { %>checked="checked"<% } %> ' +
+        '>'
+    );
+
+    var rangeWidget = _.template(
+        '<label for="slider-<%- id %>">Input slider:</label>' +
+        '<input type="range" name="<%= name %>" id="slider-<%- id %>" value="<%- value %>" min="<%- min %>" max="<%- max %>">'
+    );
+
+    var formatEdge = function(edge) {
+        return {
+            id: edge.nodeId,
+            answer: {
+                value: edge.value,
+                id: edge.edgeId
             }
-        });
-
-        return answers.join(', ');
+        };
     };
 
     var addRecordTreeAnswers = function(e, annotation) {
-        var field = {
+        var field;
+
+        field = {
             id: dtreeId,
             label: 'Decision tree'
         };
 
-        field.dtree = answers;
-        field.val = dtreeToStr(answers);
+        field.dtree = eoGraph.edges().map(formatEdge);
+        field.val = eoGraph.values().join(', ');
 
         annotation.record.properties.fields.push(field);
     };
 
-    /**
-     * function for appending the question to the html
-     * @param q is the question object
-     * @param choice is the answer if one exists
-     */
-    var createQuestion = function(q, choice){
-        if(q){
-            $('#dtree-form').html(createQuestionOptions(q, choice).join("")).parent().trigger('create');
-        }
-    };
+    var renderFieldsToString = function(name, edges) {
+        var elementType;
+        var fields = '';
+        var edge;
+        edges = edges || [];
 
-    /**
-     * function for generating the questions
-     * @param obj is the question object
-     * @param choice is the answer if already exists (for when user clicks prev)
-     * @returns html fieldcontain that is appended
-     */
-    var createQuestionOptions = function(obj, choice){
-        var fieldset;
-        if(obj.children instanceof Array) {
-            fieldset = ['<fieldset data-role="controlgroup">'];
-            fieldset.push('<legend>' + obj.name + '</legend>');
-            $.each(obj.children, function(i,v){
-                var id;
-                if(v[1] !== null){
-                    fieldset.push('<label for="'+v[1]+'">' + v[0] + '</label>');
-                    id = v[1];
-                    if(choice && choice === v[0]){
-                        fieldset.push('<input name="radio-' + page + '" id="' + id + '" value="' + v[0] + '" type="radio" required="" checked="checked">');
-                    }
-                    else{
-                        fieldset.push('<input name="radio-' + page + '" id="' + id + '" value="' + v[0] + '" type="radio" required="">');
-                    }
-                }
-                else{
-                    fieldset.push('<label for="'+v[0]+'">' + v[0] + '</label>');
-                    id = v[0];
-                    if(choice && choice === v[0]){
-                        fieldset.push('<input name="radio-' + page + '" id="' + id + '" value="' + v[0] + '" type="radio" required="" checked="checked">');
-                    }
-                    else{
-                        fieldset.push('<input name="radio-' + page + '" id="' + id + '" value="' + v[0] + '" type="radio" required="">');
-                    }
-                }
-            });
-            fieldset.push('</div>');
-        }
-        else if(obj.children === 'integer'){
-            fieldset = ['<label for="'+ page +'">'+obj.name+'</label>'];
-            fieldset.push('<input type="number" id="'+page+'" value="0">');
-        }
-        return fieldset;
-    };
+        for (var i = 0, len = edges.length; i < len; i++) {
+            edge = edges[i];
+            if (edge.element) {
+                elementType = edge.element.type;
+            }
+            else {
+                elementType = 'radio';
+            }
 
-    /**
-     * Checks that i) something is selected ii) answer is implemented
-     *  and shows relevant message.
-     * Returns true/false to indicate correct/incorrect result
-     */
-    var checkAnswer = function(){
-        var selected = getSelection();
-        var answer = questions[page].children;
-        if (! selected ){ //no selection made
-            popup("Please choose a class");
-            return false;
-        }
-        return true;
-    };
-
-    /**
-     * function for checking answers inside the array if already exist
-     * @param back is boolean to show if the user goes backwards
-     * @returns valueif there is one
-     */
-    var checkAnswerInAnswers = function(back){
-        var value;
-        var changed = false;
-        for(var i=answers.length-1; i>=0;i--){
-            if(answers[i].id === page){
-                if(back){
-                    page = answers[i-1].id;
-                    value = answers[i-1].answer.value;
-                }
-                else{
-                    page = answers[i].id;
-                    value = answers[i].answer.value;
-                }
-                changed = true;
+            switch (elementType) {
+                case 'slider':
+                    fields += rangeWidget({
+                        id: i,
+                        name: name,
+                        label: edge.label,
+                        min: edge.element.options.min,
+                        max: edge.element.options.max,
+                        value: edge.element.options.value
+                    });
                 break;
+                default:
+                    fields += radioWidget({
+                        id: i,
+                        name: name,
+                        label: edge.label,
+                        required: false,
+                        checked: false,
+                        value: edge.value
+                    });
             }
         }
-        if(!changed && answers.length > 0 && back){
-            page = answers[answers.length-1].id;
-            value = answers[answers.length-1].answer.value;
-        }
-        return value;
+
+        return fields;
     };
 
-    /**
-     * function for finding next question
-     * @param answer leads us to the next question
-     */
-    var findNextQuestion = function(answer){
-        if("leadsTo" in questionObj){
-            var leadsTo = questionObj.leadsTo;
-            var oldPage = page;
-            if(leadsTo instanceof Array){
-                //check if there's end in the leadsTo array
-                if(leadsTo.indexOf("end") > -1){
-                    for (var k=0; k<leadsTo.length-1; k++){
-                        //if there is _ it means we have potential answers to check for
-                        if(leadsTo[k].indexOf("_") > -1){
-                            //find the potential next pages (answers from before)
-                            var pages = leadsTo[k].split("_");
-                            for(var i=0; i<answers.length; i++){
-                                for(var j=0; j<pages.length; j++){
-                                    //if one of the potential next questions exists as answer
-                                    if(pages[j]  === answers[i].answer.id){
-                                        page = leadsTo[k];
-                                        break;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    //if page from above not found then end it
-                    if(oldPage === page){
-                        page = leadsTo[leadsTo.length-1];
-                    }
-                }
-                else{
-                    //check if any of the previous answers is in one of the potential destinations
-                    for(var l=0; l<answers.length; l++){
-                        for(var m=0; m<leadsTo.length; m++){
-                            //if there's - that means that there are answers that lead to the same page
-                            if(leadsTo[m].indexOf("-") && answers[l].answer.id.split("-")[0] === leadsTo[m]){
-                                page = answers[l].answer.id.split("-")[0];
-                            }
-                            //if there's an answer with the potentail next page then that's the page
-                            else if(answers[l].answer.id === leadsTo[m]){
-                                page = answers[l].answer.id;
-                            }
-                        }
-                    }
-                    if(oldPage === page){
-                        page = leadsTo[leadsTo.length-1];
-                    }
-                }
-            }
-            else{
-                page = questionObj.leadsTo;
-            }
-        }
-        else{
-            page = answer.id;
-        }
+    var renderFieldSetToString = function(node) {
+        return fieldsetWidget({
+            question: node.label,
+            fields: renderFieldsToString(node.name, node.edges)
+        });
     };
 
-    /** Returns selected value or null if none selected */
-    var getSelection = function(){
-        var selected;
-        if(questions[page].children instanceof Array){
-            selected = {
-                "value": $('input[name="radio-' + page + '"]:checked', '#dtree-form').val(),
-                "id": $('input[name="radio-' + page + '"]:checked', '#dtree-form').prop('id')
-            };
-        }
-        else{
-            selected = {
-                "value": $('input').val(),
-                "id": page
-            };
-        }
-        if(selected.value === undefined){
-            return null;
-        }
-        else{
-            return selected;
-        }
-    };
+    var renderPage = function() {
+        var node;
+        var html;
 
-    /**
-     * function for getting the question from data
-     * @param key: the id of the question
-     * @returns: the question object
-     */
-    var getQuestion = function(key){
-        for(var q in questions){
-            if(q === key){
-                return questions[q];
-            }
-        }
-        return;
-    };
+        node = eoGraph.current();
 
-    /**
-     * function for initializing the questionnaire
-     */
-    var initQuestionnaire = function(){
-        var q = getQuestion('root');
-        if(q){
-            setQuestion(q);
-            createQuestion(q);
-        }
+        html = renderFieldSetToString(node);
+        $('#dtree-form')
+            .html(html)
+            .parent()
+            .trigger('create');
     };
 
     /* popup a msg */
-    var popup = function(msg){
+    var popup = function(msg) {
         $('#popup-text').text(msg).parent().popup('open');
-    };
-
-    var setQuestion = function(q){
-        questionObj = q;
     };
 
     /**
      * function for initializing DecisionTree
      * download data and start questionnaire
      */
-    var initDtree = function(){
-        var group = localStorage.getItem('annotate-form-group');
-        var type = localStorage.getItem('annotate-form-type');
-
-        group = group || records.EDITOR_GROUP.DEFAULT;
-        answers=[];
-        page = "root";
-
+    var initDtree = function(group, type) {
         var url;
 
-        if(group ===  records.EDITOR_GROUP.DEFAULT){
+        if (group ===  records.EDITOR_GROUP.DEFAULT) {
             url = 'editors/' + type.replace(/.edtr$/, '.json');
-        }else{
+        }
+        else {
             url = file.getFilePath(records.getEditorsDir(group)) + '/' + type.replace(/.edtr$/, '.json');
         }
         dgroup = group;
@@ -312,16 +173,20 @@ define(['records', 'utils', 'file'], function(records, utils, file){
 
         $.ajax({
             url: url,
-            dataType: "json",
-            success: function(data){
+            dataType: 'json',
+            success: function(data) {
                 $.mobile.loading('hide');
-                console.log(data);
-                if(data){
-                    questions = data;
-                    $('body').pagecontainer('change', 'decision-tree.html', {transition: "fade"});
+                console.debug(data);
+                eoGraph = new EOGraph();
+                if (data) {
+                    if (!eoGraph.init(data)) {
+                        utils.informError('Error loading the tree');
+                    }
+
+                    $('body').pagecontainer('change', 'decision-tree.html', {transition: 'fade'});
                 }
-                else{
-                    utils.informError("No data ");
+                else {
+                    utils.informError('No data ');
                 }
             }
         });
@@ -334,14 +199,14 @@ define(['records', 'utils', 'file'], function(records, utils, file){
      * @param group from records.EDITOR_GROUP
      * @param online boolean value if the processing is held online
      */
-    var processEditor = function(editorName, html, group, online){
+    var processEditor = function(editorName, html, group, online) {
         var $form = $(html);
         var editorsObj = records.loadEditorsMetadata();
 
         // Add the dom class that will be used in the buttons
         var editorClass = $('#dtree-form-class-name', $form).text();
-        if(editorClass !== ""){
-            if(editorsObj[group][editorName] === undefined){
+        if (editorClass !== '') {
+            if (editorsObj[group][editorName] === undefined) {
                 editorsObj[group][editorName] = {};
             }
 
@@ -372,50 +237,27 @@ define(['records', 'utils', 'file'], function(records, utils, file){
     $(document).on(
         'vclick',
         '#dtree-prev',
-        function(event){
-            previous++;
-            var value = checkAnswerInAnswers(true);
-            createQuestion(questions[page], value);
-            setQuestion(questions[page]);
+        function(event) {
+            eoGraph.prev();
+            renderPage();
         }
     );
     $(document).on(
         'vclick',
         '#dtree-next',
-        function(){
-            //check if answer is selected
-            if(!checkAnswer()){
-                $("#dtree-popup").popup('open');
-                return;
-            }
-            var answer = getSelection();
-
-            var check =false;
-            for(var i=0; i<answers.length;i++){
-                if(answers[i].id === page){
-                    if(answer.value !== answers[i].answer.value){
-                        answers[i].answer = answer;
-                    }
-                    check = true;
-                    break;
+        function() {
+            var answers = $('form').serializeArray();
+            if (answers.length === 1) {
+                eoGraph.next(answers[0].value);
+                if (eoGraph.hasNext()) {
+                    renderPage();
+                }
+                else {
+                    records.annotate(dgroup, dtype);
                 }
             }
-            if(check === false){
-                //store the answer in an array
-                answers.push({"id": page, "answer": answer});
-            }
-            findNextQuestion(answer);
-            //get answer for the question if already exists
-            var value = checkAnswerInAnswers();
-
-            //if the page is not the end render the question
-            if(page !== "end"){
-                createQuestion(questions[page], value);
-                setQuestion(questions[page]);
-            }
-            //if page==end then fire up final form
-            else{
-                records.annotate(dgroup, dtype);
+            else {
+                $('#dtree-popup').popup('Please answer all the questions');
             }
         }
     );
@@ -424,21 +266,30 @@ define(['records', 'utils', 'file'], function(records, utils, file){
     $('.annotate-dtree').unbind();
     $('body').on('click', '.annotate-dtree', function(event) {
         var fieldcontain = $(event.target).closest('.fieldcontain').get(0);
+        var group = localStorage.getItem('annotate-form-group');
+        var type = localStorage.getItem('annotate-form-type');
 
         if (fieldcontain && fieldcontain.id) {
             dtreeId = fieldcontain.id;
         }
-        initDtree();
+
+        initDtree(group, type);
     });
 
     // Dtree from the the capture page
     $('.annotate-dtree-form').unbind();
     $('body').on('click', '.annotate-dtree-form', function(event) {
-        initDtree();
+        var $editor = $(event.currentTarget);
+        var group = $editor.attr('data-editor-group');
+        var type = $editor.attr('data-editor-type');
+
+        group = group || records.EDITOR_GROUP.DEFAULT;
+
+        initDtree(group, type);
     });
 
     // listen on any page with class sync-page
-    $(document).on('_pageshow', '#decision-tree-page', initQuestionnaire);
+    $(document).on('_pageshow', '#decision-tree-page', renderPage);
     $(document).on(records.EVT_EDIT_ANNOTATION, addRecordTreeAnswers);
 
     $('head').prepend('<link rel="stylesheet" href="plugins/sync/css/style.css" type="text/css" />');
